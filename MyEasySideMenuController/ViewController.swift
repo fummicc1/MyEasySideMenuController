761f0db5d8d38371c1e0cdb6425f7f1924ea90d6
fummicc1 <fumiya.tennis1@gmail.com>
2019-05-07 02:34:46 +0900

    Initial Commit

---
+++ MyEasySideMenuController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyEasySideMenuController
+//
+//  Created by Fumiya Tanaka on 2019/05/07.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
